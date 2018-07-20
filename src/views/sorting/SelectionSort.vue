1a009bc3a8b7ee83388ad98f50d645995c3e2464
原家彤 <yuanjiatong@zhongbiao.mobi>
2018-07-20 09:55:23 +0800

    draw bar plot

--- src/views/sorting/SelectionSort.vue
+++ src/views/sorting/SelectionSort.vue
@@ -19,22 +19,57 @@ import * as d3 from 'd3'
 // svg size
 const SVG_WIDTH = 960
 const SVG_HEIGHT = 800
+// space between bars
+const BAR_GAP = 10
+const BOTTOM = SVG_HEIGHT * 7 / 8
 
 export default {
     data () {
         return {
+            dataSet: [
+                110, 199, 4, 48, 190, 32, 148, 161, 36, 177, 115, 30, 58, 197, 72, 134, 107, 143, 28, 81,
+            ],
             isSorting: false,
         }
     },
     mounted () {
-        this.drawLabels()
+        this.drawBar()
     },
     methods: {
-        drawLabels () {
+        drawBar () {
+            const barWidth = (SVG_WIDTH - BAR_GAP) / this.dataSet.length
             let svg = d3.select('#selectionsort')
                 .attr('width', SVG_WIDTH)
                 .attr('height', SVG_HEIGHT)
-            svg.append('g')
+
+            let bar = svg.selectAll('rect')
+                .data(this.dataSet, (d) => { return d })
+
+            let yScale = d3.scaleLinear()
+                .domain([0, d3.max(this.dataSet)])
+                .range([0, SVG_HEIGHT * 0.5])
+
+            let barEnter = bar.enter()
+
+            barEnter.append('rect')
+                .attr('width', barWidth - BAR_GAP)
+                .attr('height', (d) => { return yScale(d) })
+                .attr('x', (d, i) => {
+                    return barWidth * i + BAR_GAP
+                })
+                .attr('y', (d) => {
+                    return BOTTOM - yScale(d)
+                })
+
+            barEnter.append('text')
+                .text((d) => { return d })
+                .attr('text-anchor', 'start')
+                .attr('x', (d, i) => {
+                    return barWidth * i + BAR_GAP
+                })
+                .attr('y', (d) => {
+                    return BOTTOM - yScale(d)
+                })
         },
         handleSort () {
 
