25ce3de2bf4eb50443ef2afa0948ed7cb9a6ea55
原家彤 <yuanjiatong@zhongbiao.mobi>
2018-08-06 18:33:56 +0800

    Selection sort

--- src/views/sorting/SelectionSort.vue
+++ src/views/sorting/SelectionSort.vue
@@ -9,6 +9,19 @@
             >
                 Sort
             </el-button>
+            <el-button
+                type="primary"
+                @click="handleShuffle"
+                :disabled="isSorting"
+            >
+                Shuffle
+            </el-button>
+            <div class="notes">
+                Swap count: {{ swapCount }}
+                <ol>
+                    <li v-for="(note, index) in notes" :key="index">{{ note }}</li>
+                </ol>
+            </div>
         </div>
     </div>
 </template>
@@ -25,13 +38,12 @@ const SVG_HEIGHT = 800
 const BAR_GAP = 10
 const BAR_COUNT = 20
 // arrow length
-const ARROW_LENGTH = 70
-const TEXT_DY = -10
+const ARROW_LENGTH = 50
 // transition time
 const DURATION = 800
 
 const BAR_WIDTH = (SVG_WIDTH - BAR_GAP) / BAR_COUNT - BAR_GAP
-const BAR_BOTTOM = SVG_HEIGHT * 15 / 16
+const BAR_BOTTOM = SVG_HEIGHT * 9 / 16
 
 function getX (index) {
     return BAR_GAP + (BAR_WIDTH + BAR_GAP) * index
@@ -46,118 +58,33 @@ export default {
             isSorting: false,
             svgElement: null,
             taskQueue: [],
+            swapCount: 0,
+            notes: [
+                'Running time is insensitive to input',
+                'Data movement is minimal: use N exchanges',
+            ],
         }
     },
     mounted () {
-        this.init()
+        this.drawSigns()
         this.drawBar()
+        this.drawComparison()
     },
     mixins: [drawArrow],
     methods: {
-        init () {
+        drawSigns () {
             this.svgElement = d3.select('#selectionsort')
                 .attr('width', SVG_WIDTH)
                 .attr('height', SVG_HEIGHT)
 
-            let temp = this.svgElement.append('g')
-            for (let i = 1; i < 4; i += 1) {
-                temp.append('line')
-                    .attr('x1', 0)
-                    .attr('x2', SVG_WIDTH)
-                    .attr('y1', SVG_HEIGHT * i / 8)
-                    .attr('y2', SVG_HEIGHT * i / 8)
-            }
-            // element for i
-            let iteratorI = this.svgElement.append('g')
-                .attr('class', 'iteratorI')
-            iteratorI.append('text')
-                .attr('x', 0)
-                .attr('y', SVG_HEIGHT / 8)
-                .attr('dy', TEXT_DY)
-                .attr('text-anchor', 'middle')
-                .text('i')
-            this.drawArrow({
-                element: iteratorI,
-                startX: 0,
-                endX: 0,
-                startY: SVG_HEIGHT / 8,
-                endY: SVG_HEIGHT / 8 + ARROW_LENGTH,
-            })
-            // element for min
-            let minimum = this.svgElement.append('g')
-                .attr('class', 'minimum')
-            minimum.append('text')
-                .attr('x', 0)
-                .attr('y', SVG_HEIGHT / 4)
-                .attr('dy', TEXT_DY)
-                .attr('text-anchor', 'middle')
-                .text('min')
-            this.drawArrow({
-                element: minimum,
-                startX: 0,
-                endX: 0,
-                startY: SVG_HEIGHT / 4,
-                endY: SVG_HEIGHT / 4 + ARROW_LENGTH,
-            })
-            // element for j
-            let iteratorJ = this.svgElement.append('g')
-                .attr('class', 'iteratorJ')
-            iteratorJ.append('text')
-                .attr('x', 0)
-                .attr('y', SVG_HEIGHT / 4)
-                .attr('dy', TEXT_DY)
-                .attr('text-anchor', 'middle')
-                .text('j')
-            this.drawArrow({
-                element: iteratorJ,
-                startX: 0,
-                endX: 0,
-                startY: SVG_HEIGHT / 4,
-                endY: SVG_HEIGHT / 4 + ARROW_LENGTH,
-            })
-            this.updateArrow('iteratorI', 0)
-            this.updateArrow('minimum', 0)
-            this.updateArrow('iteratorJ', 1)
-        },
-        updateArrow (className, value) {
-            const x = getX(value) + BAR_WIDTH * 0.5
-            this.svgElement.select(`g.${className}`)
-                .transition()
-                .duration(DURATION)
-                .attr('transform', `translate(${x}, 0)`)
-        },
-        drawBar () {
-            let bar = this.svgElement.selectAll('rect')
-                .data(this.dataSet, (d) => { return d })
-
-            let yScale = d3.scaleLinear()
-                .domain([0, d3.max(this.dataSet)])
-                .range([0, SVG_HEIGHT * 0.5])
-
-            let barEnter = bar.enter()
-
-            barEnter.append('rect')
-                .attr('width', BAR_WIDTH)
-                .attr('height', (d) => { return yScale(d) })
-                .attr('x', (d, i) => {
-                    return getX(i)
-                })
-                .attr('y', (d) => {
-                    return BAR_BOTTOM - yScale(d)
-                })
-
-            barEnter.append('text')
+            // add a number for each bar, indicating the index of the bar in the data set
+            this.svgElement.append('g')
+                .selectAll('text.index')
+                .data(d3.range(this.dataSet.length))
+                .enter()
+                .append('text')
+                .attr('class', 'index')
                 .text((d) => { return d })
-                .attr('text-anchor', 'start')
-                .attr('x', (d, i) => {
-                    return getX(i)
-                })
-                .attr('y', (d) => {
-                    return BAR_BOTTOM - yScale(d)
-                })
-
-            barEnter.append('text')
-                .text((d, i) => { return i })
                 .attr('text-anchor', 'start')
                 .attr('x', (d, i) => {
                     return getX(i)
@@ -165,50 +92,253 @@ export default {
                 .attr('y', () => {
                     return BAR_BOTTOM
                 })
+
+            this.svgElement.append('g')
+                .selectAll('line')
+                .data([11, 13])
+                .enter()
+                .append('line')
+                .attr('x1', 0)
+                .attr('x2', SVG_WIDTH)
+                .attr('y1', (d) => { return SVG_HEIGHT * d / 16 })
+                .attr('y2', (d) => { return SVG_HEIGHT * d / 16 })
+
+            function addSign (svg, className, x, y, text, startY, arrowFunc) {
+                let element = svg.append('g')
+                    .attr('class', className)
+                element.append('text')
+                    .attr('x', x)
+                    .attr('y', y)
+                    .attr('dy', 20)
+                    .attr('text-anchor', 'middle')
+                    .text(text)
+                arrowFunc({
+                    element,
+                    startX: 0,
+                    endX: 0,
+                    startY,
+                    endY: startY - ARROW_LENGTH,
+                })
+            }
+            // element for i
+            addSign(this.svgElement, 'iteratorI', 0, SVG_HEIGHT * 13 / 16, 'i', SVG_HEIGHT * 13 / 16, this.drawArrow)
+            // element for min
+            addSign(this.svgElement, 'minimum', 0, SVG_HEIGHT * 11 / 16, 'min', SVG_HEIGHT * 11 / 16, this.drawArrow)
+            // element for j
+            addSign(this.svgElement, 'iteratorJ', 0, SVG_HEIGHT * 11 / 16, 'j', SVG_HEIGHT * 11 / 16, this.drawArrow)
+            // move them to their initial positions
+            this.updateArrow({ className: 'iteratorI', x: 0 })
+            this.updateArrow({ className: 'minimum', x: 0 })
+            this.updateArrow({ className: 'iteratorJ', x: 1 })
+        },
+        drawBar () {
+            let bar = this.svgElement.selectAll('svg.bar')
+                .data(this.dataSet, (d) => { return d })
+
+            let yScale = d3.scaleLinear()
+                .domain([0, d3.max(this.dataSet)])
+                .range([0, SVG_HEIGHT * 0.5])
+
+            let barEnter = bar.enter()
+                .append('svg')
+                .attr('class', 'bar')
+                .attr('width', BAR_WIDTH)
+                .attr('height', SVG_HEIGHT)
+                .attr('x', (d, i) => {
+                    return getX(i)
+                })
+
+            barEnter.append('rect')
+                .attr('width', BAR_WIDTH)
+                .attr('height', (d) => { return yScale(d) })
+                .attr('x', 0)
+                .attr('y', (d) => {
+                    return BAR_BOTTOM - yScale(d)
+                })
+
+            barEnter.append('text')
+                .attr('class', 'valueText')
+                .text((d) => { return d })
+                .attr('text-anchor', 'start')
+                .attr('x', 0)
+                .attr('y', (d) => {
+                    return BAR_BOTTOM - yScale(d)
+                })
+                .attr('dy', -6)
+        },
+        drawComparison () {
+            /*
+             * Add some texts showing the comparison between minimum and j
+             * They should look like the following:
+             * minimum     j
+             *    vmin  >  vj
+             */
+            let initialMin = this.dataSet[0]
+            let initialJ = this.dataSet[1]
+            let comparison = initialMin < initialJ ? '<' : '>'
+            let dx = 20
+            let dy = '1.5em'
+            let compareElement = this.svgElement.append('g')
+                .attr('class', 'comparison')
+
+            // minimum on the left
+            compareElement.append('text')
+                .attr('x', SVG_WIDTH / 2)
+                .attr('y', SVG_HEIGHT * 15 / 16)
+                .attr('text-anchor', 'end')
+                .attr('dx', -dx)
+                .text('minimum')
+            compareElement.append('text')
+                .attr('class', 'minimumText')
+                .attr('x', SVG_WIDTH / 2)
+                .attr('y', SVG_HEIGHT * 15 / 16)
+                .attr('text-anchor', 'end')
+                .attr('dx', -dx)
+                .attr('dy', dy)
+                .text(`${initialMin}`)
+
+            // j on the right
+            compareElement.append('text')
+                .attr('x', SVG_WIDTH / 2)
+                .attr('y', SVG_HEIGHT * 15 / 16)
+                .attr('text-anchor', 'start')
+                .attr('dx', dx)
+                .text('j')
+            compareElement.append('text')
+                .attr('class', 'jText')
+                .attr('x', SVG_WIDTH / 2)
+                .attr('y', SVG_HEIGHT * 15 / 16)
+                .attr('text-anchor', 'start')
+                .attr('dx', dx)
+                .attr('dy', dy)
+                .text(`${initialJ}`)
+
+            // < or > in the middle
+            compareElement.append('text')
+                .attr('class', 'comparisonText')
+                .attr('x', SVG_WIDTH / 2)
+                .attr('y', SVG_HEIGHT * 15 / 16)
+                .attr('text-anchor', 'middle')
+                .attr('dy', dy)
+                .text(`${comparison}`)
+        },
+        updateArrow ({ className, x }) {
+            // This function moves an arrow (with the text above it) to a new position
+            // className: 'iteratorI', 'iteratorJ' or 'minimum'
+            const posX = getX(x) + BAR_WIDTH * 0.5
+            this.svgElement.select(`g.${className}`)
+                .transition()
+                .duration(DURATION)
+                .attr('transform', `translate(${posX}, 0)`)
+        },
+        updateBar ({ i1, i2, v1, v2 }) {
+            // This function swaps two bars
+            // i1, i2 are the index of these two bars in the data set
+            // v1, v2 are the corresponding values
+            function moveBar (svg, datum, index) {
+                // datum: to find the svg element to move
+                // index: to find the final position for the svg element
+                svg.selectAll('svg.bar')
+                    .filter((d) => { return d === datum })
+                    .transition()
+                    .duration(DURATION)
+                    .attr('x', () => { return getX(index) })
+            }
+            moveBar(this.svgElement, v1, i2)
+            moveBar(this.svgElement, v2, i1)
+        },
+        updateComparison ({ vMin, vJ }) {
+            const comparison = vMin < vJ ? '<' : '>'
+            this.svgElement.select('text.minimumText')
+                .text(vMin)
+            this.svgElement.select('text.jText')
+                .text(vJ)
+            this.svgElement.select('text.comparisonText')
+                .text(comparison)
         },
         handleSort () {
-            this.isSorting = true
+            this.isSorting = true // disable the buttons
             let minimum
+            // loop through the array
             for (let i = 0; i < BAR_COUNT; i += 1) {
+                // mark the index of the smallest number in the array
                 minimum = i
-                this.taskQueue.push({
-                    className: 'iteratorI',
-                    value: i,
-                })
-                this.taskQueue.push({
-                    className: 'minimum',
-                    value: i,
-                })
+                // The three arrows are already in the right positions for the first loop, so skip this task
+                if (i > 0) {
+                    // put the three arrows to the starting position before a loop begins
+                    this.taskQueue.push([
+                        { type: 'move', className: 'iteratorI', x: i },
+                        { type: 'move', className: 'minimum', x: i },
+                        // move j here instead of inside the inner loop, so that the three arrows can move simultaneously
+                        { type: 'move', className: 'iteratorJ', x: i + 1 },
+                    ])
+                }
                 for (let j = i + 1; j < BAR_COUNT; j += 1) {
-                    this.taskQueue.push({
-                        className: 'iteratorJ',
-                        value: j,
-                    })
-                    if (this.dataSet[j] < this.dataSet[minimum]) {
+                    if (j > i + 1) { // the task for (j = i + 1) has already been added before the inner loop begins
+                        this.taskQueue.push([
+                            { type: 'move', className: 'iteratorJ', x: j },
+                        ])
+                    }
+                    if (this.dataSet[j] < this.dataSet[minimum]) { // finds a smaller number
                         minimum = j
-                        this.taskQueue.push({
-                            className: 'minimum',
-                            value: j,
-                        })
+                        this.taskQueue.push([
+                            { type: 'move', className: 'minimum', x: j },
+                        ])
                     }
                 }
-                let temp = this.dataSet[i]
-                this.dataSet[i] = this.dataSet[minimum]
-                this.dataSet[minimum] = temp
+                // After the inner loop is completed, we get the smallest number for this (outer) loop
+                // Move it to its final position
+                if (i !== minimum) {
+                    let v1 = this.dataSet[i]
+                    let v2 = this.dataSet[minimum]
+                    this.dataSet[i] = v2
+                    this.dataSet[minimum] = v1
+                    this.taskQueue.push([ // Add a task to swap their corresponding bars
+                        {
+                            type: 'swap',
+                            i1: i,
+                            i2: minimum,
+                            v1,
+                            v2,
+                        },
+                    ])
+                }
             }
+
+            // The sorting is completed. Execute the tasks to the simulate the process
             let that = this
             function delay () {
+                let actions = that.taskQueue.shift() // get a new action list
+                for (let n = 0; n < actions.length; n += 1) { // there might be multiple actions
+                    let action = actions[n]
+                    if (action.type === 'move') { // move the arrows
+                        that.updateArrow(action)
+                    } else if (action.type === 'swap') { // move the bars
+                        that.updateBar(action)
+                        that.swapCount += 1
+                    }
+                }
                 setTimeout(() => {
-                    let { className, value } = that.taskQueue.shift()
-                    that.updateArrow(className, value)
-                    if (that.taskQueue.length > 0) {
-                        delay()
+                    if (that.taskQueue.length > 0) { // there are still tasks left
+                        delay() // After a timeout, run this function again to execute the next task
                     } else {
-                        this.isSorting = false
+                        that.isSorting = false // All tasks are completed. Enable the buttons
                     }
                 }, DURATION)
             }
             delay()
+        },
+        handleShuffle () {
+            for (let i = BAR_COUNT - 1; i > 0; i -= 1) {
+                let j = Math.floor(Math.random() * (i + 1))
+                let temp = this.dataSet[i]
+                this.dataSet[i] = this.dataSet[j]
+                this.dataSet[j] = temp
+            }
+            d3.selectAll('svg.bar').remove() // remove all bars
+            this.drawBar() // re-draw based on the new data set
+            d3.selectAll('g.comparison').remove() // remove comparison texts
+            this.drawComparison()
         },
     },
 }
@@ -232,11 +362,23 @@ export default {
     .buttons {
         display: flex;
         flex-direction: column;
-        align-items: center;
         width: 150px;
+        margin-left: 20px;
         .el-button {
-            margin: 10px;
             width: 100px;
+            margin-top: 10px;
+            margin-left: 0;
+        }
+        div {
+            margin-top: 20px;
+        }
+        .notes {
+            ol {
+                margin: 20px 0 0 20px;
+            }
+            li {
+                margin-top: 10px;
+            }
         }
     }
 }
